49cd19ab0a757b6dca1f361253baa1f26c08f982
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-02-17 16:52:37 -0500

    Adds support for creating TwilioProvider at startup

--- src/main/java/com/maldahleh/smsminecraft/SMSMinecraft.java
+++ src/main/java/com/maldahleh/smsminecraft/SMSMinecraft.java
@@ -1,10 +1,21 @@
 package com.maldahleh.smsminecraft;
 
+import com.maldahleh.smsminecraft.exceptions.MissingConfigException;
+import com.maldahleh.smsminecraft.twilio.TwilioProvider;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
 public class SMSMinecraft extends JavaPlugin {
+    private TwilioProvider twilioProvider;
+
     @Override
     public void onEnable() {
-
+        try {
+            twilioProvider = new TwilioProvider(getConfig().getConfigurationSection("twilio"));
+        } catch (MissingConfigException e) {
+            getLogger().severe("Plugin shutting down; error=" + e.getMessage());
+            getPluginLoader().disablePlugin(this);
+        }
     }
 }
